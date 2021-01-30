c965613e7f74da2437d1f62fb5510b88ca064d2d
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-01-30 01:58:17 -0300

    script base created

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-module.exports = function (req, res) {
-    return function () {
+module.exports = function (data) {
 
-    };
+    // Prepare Functions
+    let functions = null;
+    try {
+        functions = require('firebase-functions');
+    } catch (err) {
+        functions = null;
+    }
+
+    // Start Module
+    if (functions) {
+
+        // Prepare Modules
+        const _ = require('lodash');
+
+        // Create Settings
+        const tinyCfg = _.defaultsDeep({}, data, {
+            path: '/',
+            database: ''
+        });
+
+        // Script Base
+        const discordCommandChecker = (snapshot, context) => {
+
+            // Complete
+            return;
+
+        };
+
+        // Prepare Base
+        return {
+
+            onWrite: functions.database.instance(tinyCfg.database).ref(tinyCfg.path).onWrite(discordCommandChecker),
+            onCreate: functions.database.instance(tinyCfg.database).ref(tinyCfg.path).onCreate(discordCommandChecker),
+            onUpdate: functions.database.instance(tinyCfg.database).ref(tinyCfg.path).onUpdate(discordCommandChecker),
+            onDelete: functions.database.instance(tinyCfg.database).ref(tinyCfg.path).onDelete(discordCommandChecker),
+
+        };
+
+    }
+
+    // Nope
+    else {
+        return null;
+    }
+
 };
