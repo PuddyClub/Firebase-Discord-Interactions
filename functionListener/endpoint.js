f9d3288feba612c1780a5c312d1e41183b728606
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-01 19:28:01 -0300

    endpoint update

--- functionListener/endpoint.js
+++ functionListener/endpoint.js
@@ -1,47 +1,59 @@
-module.exports = async function (req, res, logger, tinyCfg) {
+module.exports = function (req, res, logger, tinyCfg) {
 
     // Start Firebase
     const firebase = require('@tinypudding/firebase-lib');
-    firebase.start(require('firebase-admin'), tinyCfg.options, tinyCfg.firebase);
+    firebase.start(require('firebase-admin'), tinyCfg.firebase.options, tinyCfg.firebase.app);
 
     // App
-    const app = firebase.get(tinyCfg.options.id);
-    let db = null;
+    const app = firebase.get(tinyCfg.firebase.options.id);
 
     // Get DB
     if (typeof req.query.bot === "string") {
-        db = app.db.ref('apps').child(firebase.databaseEscape(req.query.bot));
-    } else {
-        db = app.db.ref('bot_tester');
-    }
 
-    // Get Public Key
-    const public_key = await firebase.getDBData(db.child('public_key'));
-    if (typeof public_key === "string") {
+        // Bot Var
+        const db = app.db.ref(tinyCfg.appPath).child(firebase.databaseEscape(req.query.bot));
 
-        // Prepare Validation
-        const di = require('discord-interactions');
-        const signature = req.get('X-Signature-Ed25519');
-        const timestamp = req.get('X-Signature-Timestamp');
+        // Get Public Key
+        firebase.getDBData(db.child('public_key')).then(async public_key => {
 
-        try {
+            if (typeof public_key === "string") {
 
-            // Get Valid Request
-            const isValidRequest = await di.verifyKey(req.rawBody, signature, timestamp, public_key);
-
-            // Is Valid
-            if (isValidRequest) {
-
-                // Version Validator
-                if (typeof req.body.version !== "number" || isNaN(req.body.version) || !isFinite(req.body.version) || req.body.version < 1) {
-                    req.body.version = 1;
-                }
+                // Prepare Validation
+                const di = require('discord-interactions');
+                const signature = req.get('X-Signature-Ed25519');
+                const timestamp = req.get('X-Signature-Timestamp');
 
                 try {
-                    return require('./version/' + req.body.version)(req, res, logger, di);
+
+                    // Get Valid Request
+                    const isValidRequest = await di.verifyKey(req.rawBody, signature, timestamp, public_key);
+
+                    // Is Valid
+                    if (isValidRequest) {
+
+                        // Version Validator
+                        if (typeof req.body.version !== "number" || isNaN(req.body.version) || !isFinite(req.body.version) || req.body.version < 1) {
+                            req.body.version = 1;
+                        }
+
+                        try {
+                            return require('./version/' + req.body.version)(req, res, logger, di);
+                        } catch (err) {
+                            logger.error(err);
+                            tinyCfg.errorCallback(res, 404, 'Version not found!');
+                            return;
+                        }
+
+                    }
+
+                    // Nope
+                    else {
+                        return tinyCfg.errorCallback(res, 401, 'Bad request signature!');
+                    }
+
                 } catch (err) {
                     logger.error(err);
-                    tinyCfg.errorCallback(res, 404, 'Version not found!');
+                    tinyCfg.errorCallback(res, 500, err.message);
                     return;
                 }
 
@@ -49,20 +61,21 @@ module.exports = async function (req, res, logger, tinyCfg) {
 
             // Nope
             else {
-                return tinyCfg.errorCallback(res, 401, 'Bad request signature!');
+                tinyCfg.errorCallback(res, 401, 'Invalid Public Key!');
             }
 
-        } catch (err) {
-            logger.error(err);
-            tinyCfg.errorCallback(res, 500, err.message);
+            // Complete
             return;
-        }
+
+        }).catch(err => {
+            tinyCfg.errorCallback(res, 404, 'Bot Public Key not found!');
+        });
 
     }
 
     // Nope
     else {
-        tinyCfg.errorCallback(res, 401, 'Invalid Public Key!');
+        tinyCfg.errorCallback(res, 401, 'Invalid Bot Data!');
     }
 
     // Complete
