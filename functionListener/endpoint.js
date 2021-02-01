3dc98cea7436053d13db8c333544bd0dde790817
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-01 19:35:57 -0300

    endpoint update

--- functionListener/endpoint.js
+++ functionListener/endpoint.js
@@ -1,82 +1,113 @@
 module.exports = function (req, res, logger, tinyCfg) {
 
-    // Start Firebase
-    const firebase = require('@tinypudding/firebase-lib');
-    firebase.start(require('firebase-admin'), tinyCfg.firebase.options, tinyCfg.firebase.app);
+    // Object Type
+    const objType = require('@tinypudding/puddy-lib/get/objType');
 
     // App
-    const app = firebase.get(tinyCfg.firebase.options.id);
+    let app = null;
 
-    // Get DB
-    if (typeof req.query.bot === "string") {
+    // Exist Firebase
+    if (objType(tinyCfg.firebase)) {
 
-        // Bot Var
-        const db = app.db.ref(tinyCfg.appPath).child(firebase.databaseEscape(req.query.bot));
+        // New Firebase
+        if (objType(tinyCfg.firebase.options)) {
 
-        // Get Public Key
-        firebase.getDBData(db.child('public_key')).then(async public_key => {
+            // Start Firebase
+            const firebase = require('@tinypudding/firebase-lib');
+            firebase.start(require('firebase-admin'), tinyCfg.firebase.options, tinyCfg.firebase.app);
+            app = firebase.get(tinyCfg.firebase.options.id);
 
-            if (typeof public_key === "string") {
+        }
 
-                // Prepare Validation
-                const di = require('discord-interactions');
-                const signature = req.get('X-Signature-Ed25519');
-                const timestamp = req.get('X-Signature-Timestamp');
+        // Nope
+        else { app = tinyCfg.firebase; }
 
-                try {
+    }
 
-                    // Get Valid Request
-                    const isValidRequest = await di.verifyKey(req.rawBody, signature, timestamp, public_key);
+    // Exist Firebase
+    if (app) {
 
-                    // Is Valid
-                    if (isValidRequest) {
+        // Get DB
+        if (typeof req.query.bot === "string") {
 
-                        // Version Validator
-                        if (typeof req.body.version !== "number" || isNaN(req.body.version) || !isFinite(req.body.version) || req.body.version < 1) {
-                            req.body.version = 1;
+            // Firebase Lib
+            const databaseEscape = require('@tinypudding/firebase-lib/databaseEscape');
+            const getDBData = require('@tinypudding/firebase-lib/getDBData');
+
+            // Bot Var
+            const db = app.db.ref(tinyCfg.appPath).child(databaseEscape(req.query.bot));
+
+            // Get Public Key
+            getDBData(db.child('public_key')).then(async public_key => {
+
+                if (typeof public_key === "string") {
+
+                    // Prepare Validation
+                    const di = require('discord-interactions');
+                    const signature = req.get('X-Signature-Ed25519');
+                    const timestamp = req.get('X-Signature-Timestamp');
+
+                    try {
+
+                        // Get Valid Request
+                        const isValidRequest = await di.verifyKey(req.rawBody, signature, timestamp, public_key);
+
+                        // Is Valid
+                        if (isValidRequest) {
+
+                            // Version Validator
+                            if (typeof req.body.version !== "number" || isNaN(req.body.version) || !isFinite(req.body.version) || req.body.version < 1) {
+                                req.body.version = 1;
+                            }
+
+                            try {
+                                return require('./version/' + req.body.version)(req, res, logger, di);
+                            } catch (err) {
+                                logger.error(err);
+                                tinyCfg.errorCallback(res, 404, 'Version not found!');
+                                return;
+                            }
+
                         }
 
-                        try {
-                            return require('./version/' + req.body.version)(req, res, logger, di);
-                        } catch (err) {
-                            logger.error(err);
-                            tinyCfg.errorCallback(res, 404, 'Version not found!');
-                            return;
+                        // Nope
+                        else {
+                            return tinyCfg.errorCallback(res, 401, 'Bad request signature!');
                         }
 
+                    } catch (err) {
+                        logger.error(err);
+                        tinyCfg.errorCallback(res, 500, err.message);
+                        return;
                     }
 
-                    // Nope
-                    else {
-                        return tinyCfg.errorCallback(res, 401, 'Bad request signature!');
-                    }
-
-                } catch (err) {
-                    logger.error(err);
-                    tinyCfg.errorCallback(res, 500, err.message);
-                    return;
                 }
 
-            }
+                // Nope
+                else {
+                    tinyCfg.errorCallback(res, 401, 'Invalid Public Key!');
+                }
 
-            // Nope
-            else {
-                tinyCfg.errorCallback(res, 401, 'Invalid Public Key!');
-            }
+                // Complete
+                return;
 
-            // Complete
-            return;
+            }).catch(err => {
+                tinyCfg.errorCallback(res, 404, 'Bot Public Key not found!');
+                return;
+            });
 
-        }).catch(err => {
-            tinyCfg.errorCallback(res, 404, 'Bot Public Key not found!');
-            return;
-        });
+        }
+
+        // Nope
+        else {
+            tinyCfg.errorCallback(res, 401, 'Invalid Bot Data!');
+        }
 
     }
 
     // Nope
     else {
-        tinyCfg.errorCallback(res, 401, 'Invalid Bot Data!');
+        tinyCfg.errorCallback(res, 500, 'Main Server not found!');
     }
 
     // Complete
