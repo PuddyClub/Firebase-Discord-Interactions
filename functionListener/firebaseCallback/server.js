8fac97d64f6cf17477764c994f392124ad8c3504
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-17 12:32:52 -0300

    error update

--- functionListener/firebaseCallback/server.js
+++ functionListener/firebaseCallback/server.js
@@ -35,7 +35,11 @@ module.exports = async (functions, tinyCfg, data) => {
                 await finalResult(data, res, logger, tinyCfg, require('../validator/checker')(req, res, logger, tinyCfg));
                 return { success: true };
             } catch (err) {
-                return { success: false, error: err.message };
+                if (err && typeof err.message === "string") {
+                    return { success: false, error: err.message };
+                } else {
+                    return { success: false, error: err };
+                }
             }
 
         }
