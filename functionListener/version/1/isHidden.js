895166ebacf7c6a34f83eea5012993d2f910b4d0
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-04-23 21:18:51 -0300

    isHidden finished

--- functionListener/version/1/isHidden.js
+++ functionListener/version/1/isHidden.js
@@ -1,7 +1,36 @@
 module.exports = (data, result, getItem, tinyCfg) => {
-  
 
-    
+    // Hidden Checker
+    const hiddenChecker = (item, getItem) => {
+        if (
+            (typeof item.description === "string" && item.description.indexOf(tinyCfg.hiddenDetector.icon) > -1) ||
+            getItem.boolean(tinyCfg.hiddenDetector.value)
+        ) { return true; } else { return false; }
+    };
+
+    const tryMoreHidden = (options) => {
+        for (const item in options) {
+            if (!isHidden) { isHidden = hiddenChecker(options[item].options, getValues.createFunctions({ data: options[item] }, tinyCfg.bot)); } else { return true; }
+        }
+        return false;
+    };
+
+    // The Value
+    let isHidden = false;
+
+    // Base
+    isHidden = hiddenChecker(result.interaction.data, getItem);
+    if (!isHidden && result.interaction.data.options) {
+        isHidden = tryMoreHidden(result.interaction.data.options);
+    }
+
+    // Is Hidden
+    if (isHidden) {
+        if (typeof data === "string") { data = { content: data, flags: 64 }; } else {
+            data.data.flags = 64;
+        }
+    }
+
     return data;
-    
+
 };
